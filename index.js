5b8929481b8c3aa8800977fac8e9dff0c45ed389
Lexfer <lexferramirez@gmail.com>
2021-11-21 10:29:52 -0400

    1st commit develop

---
+++ index.js
@@ -0,0 +1,8 @@
+console.log('Hello lexfer');
+
+
+//instalacion de gitflow en ubuntu Linux 
+// wget --no-check-certificate -q -O - https://github.com/nvie/gitflow/raw/develop/contrib/gitflow-installer.sh | sudo bash
+
+
+console.log('cambio del new feature');
